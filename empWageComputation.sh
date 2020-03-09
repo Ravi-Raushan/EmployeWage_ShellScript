340d88b30c8f45b58737505a86a502be04c83f46
Ravi-Raushan <krravi015@gmail.com>
2020-03-09 13:36:34 +0530

    [Ravi Raushan]Added. Employee Hours Limit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
 echo "****** WELCOME TO EMPLOYEE WAGE COMPUTATION ******"
 WORKING_DAY_PER_MONTH=20
+TOTAL_WORKING_HRS=100
 WAGE_PER_HRS=20
 IS_FULL_TIME=1
 IS_PART_TIME=2
 
 totalWorkingDays=0;
 totalEmployeeHrs=0;
-while (( totalWorkingDays<$WORKING_DAY_PER_MONTH ))
+while (( totalEmployeeHrs<$TOTAL_WORKING_HRS && totalWorkingDays<$WORKING_DAY_PER_MONTH ))
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
